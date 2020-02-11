c480fef5e7d3bc100f3ee27dc1400cc5216747a2
Austin Montgomery <austinbrucemontgomery@gmail.com>
2020-02-11 14:40:28 -0700

    Use dplyr instead of tidyverse

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(shiny)
 library(shinydashboard)
 options(shiny.maxRequestSize=50*1024^2) 
 library(ggplot2)
-library(tidyverse)
+library(dplyr)
 library(DT)
 names <- c("1A", "1B", "2A", "2B", "3A", "3B", "4A", "4B", "5A", "5B", "6A", "6B")
 
